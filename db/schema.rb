33e9e7e8e0c858cfe8e40d335eb657e07a3f3fcc
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-07-17 03:45:30 +0000

    PostImage_modelにdefdef_get_image入力

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2022_07_05_115116) do
   end
 
   create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
